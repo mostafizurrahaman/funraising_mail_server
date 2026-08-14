f1c38b90c82f7326e3d377348762f011b53a9b66
mostafizurRahaman <mostafizurrahaman0401@gmail.com>
2026-08-14 22:08:59 +0600

    fix: changes completed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
    apps: [
       {
          name: "replica_server",
-         script: "bun run prod:start",
+         script: "npm run prod:start",
          env: {
             NODE_ENV: "production",
             PORT: 5000,
